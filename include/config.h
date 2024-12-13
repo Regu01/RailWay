57704993165cbcd699f7048ac58cb0e47a2edbc7
Regu01 <leo.peyon@gmail.com>
2024-12-13 19:03:28 +0100

    Add Config file

---
+++ include/config.h
@@ -0,0 +1,27 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// // Wi-Fi Configuration PROD
+// #define WIFI_SSID "TrainNetwork"
+// #define WIFI_PASSWORD "TrainNetwork"
+
+// // MQTT Configuration
+// #define MQTT_BROKER "192.168.137.1"
+// #define MQTT_PORT 1883
+// #define MQTT_USER "mqtt_user"
+// #define MQTT_PASSWORD "mqtt_password"
+
+// ------------------------------------ //
+
+// Wi-Fi Configuration Local
+#define WIFI_SSID "Freebox-Peyon"
+#define WIFI_PASSWORD "Peyon17t*"
+
+// MQTT Configuration
+#define MQTT_BROKER "192.168.1.167"
+#define MQTT_PORT 1883
+#define MQTT_USER "mqtt_user"
+#define MQTT_PASSWORD "mqtt_password"
+
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,21 @@
 #include <Arduino.h>
+#include "config.h"
 
 #include "wifi/wifi.h"
 #include "mqtt/MqttClient.h"
 #include "signal_blocs/SignalBlocs.h"
 
 // Déclaration des informations Wi-Fi
-const char *ssid = "TrainNetwork";
-const char *password = "TrainNetwork";
-const char *mqtt_server = "192.168.137.1";
-const char *mqtt_user = "mqtt_user";
-const char *mqtt_password = "mqtt_password";
-
-
+const char *ssid = WIFI_SSID;
+const char *password = WIFI_PASSWORD;
+const char *mqtt_server = MQTT_BROKER;
+const char *mqtt_user = MQTT_USER;
+const char *mqtt_password = MQTT_PASSWORD;
 
 
 // Initialisation du Wi-Fi et MQTT
 WifiManager wifiManager(ssid, password);
-MqttClient mqttClient(mqtt_server, 1883, mqtt_user, mqtt_password);
+MqttClient mqttClient(mqtt_server, MQTT_PORT, mqtt_user, mqtt_password);
 
 // Initialisation du module SignalBlocs
 SignalBlocs signalBlocs(1, &mqttClient);
